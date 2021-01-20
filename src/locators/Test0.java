2e616640f104bf5a3b7ecda096894cab8cd69bed
QSP <QSP@DESKTOP-EVK9GMC>
2021-01-20 07:28:33 +0530

    Locators

---
+++ src/locators/Test0.java
@@ -0,0 +1,22 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		
+		//driver.findElement(By.linkText("Forgot your password?")).click();
+		
+		driver.findElement(By.partialLinkText("password?")).click();
+
+	}
+
+}
