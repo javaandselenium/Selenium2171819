3188205e964364a1479b267d02e8ff4384d2022a
QSP <QSP@DESKTOP-EVK9GMC>
2021-01-21 07:24:52 +0530

    CSS

---
+++ src/navigation/Test1.java
@@ -0,0 +1,24 @@
+package navigation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(3000);
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+		driver.close();
+
+	}
+
+}
